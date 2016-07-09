84b6956766470c8acd11a59b663babaffd905ca0
Juno_okyo <J2TeaM@users.noreply.github.com>
2016-07-10 01:28:04 +0700

    Fix "Undefined index" errors

--- index.php
+++ index.php
@@ -12,14 +12,15 @@ $allow_delete = true;
 /*
 $PASSWORD = 'sfm'; 
 session_start();
-if(!$_SESSION['_sfm_allowed']) {
+if( ! isset($_SESSION['_sfm_allowed'])) {
 	// sha1, and random bytes to thwart timing attacks.  Not meant as secure hashing.
 	$t = bin2hex(openssl_random_pseudo_bytes(10));	
-	if($_POST['p'] && sha1($t.$_POST['p']) === sha1($t.$PASSWORD)) {
+	if(isset($_POST['p']) && sha1($t.$_POST['p']) === sha1($t.$PASSWORD)) {
 		$_SESSION['_sfm_allowed'] = true;
 		header('Location: ?');
+		exit;
 	}
-	echo '<html><body><form action=? method=post>PASSWORD:<input type=password name=p /></form></body></html>'; 
+	echo '<html><body><form action=? method=post>PASSWORD: <input type=password name=p autofocus required></form></body></html>'; 
 	exit;
 }
 */
