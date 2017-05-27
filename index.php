d18e94beb7ba7493a355373110d9660cbbad3501
dvbava <dvbava@yahoo.com>
2017-05-27 10:42:39 +0100

    Update index.php
    allow file manager to use arbitrary location as root folder, even out side web folder.
    tidy up HTML
    tidy up login - logout

--- index.php
+++ index.php
@@ -15,131 +15,165 @@ $allow_create_folder = true; // Set to false to disable folder creation
 $allow_upload = true; // Set to true to allow upload files
 $allow_direct_link = true; // Set to false to only allow downloads and not direct link
 
+// configure to any location.
+$rootFolder =  '/home/username/fm-data-files';
+$timeZone = 'Europe/London';
+
 $disallowed_extensions = ['php'];  // must be an array. Extensions disallowed to be uploaded
 
 $hidden_extensions = ['php']; // must be an array. Extensions hidden in directory index
 
 $PASSWORD = '';  // Set the password, to access the file manager... (optional)
 
+date_default_timezone_set($timeZone);
+
 if($PASSWORD) {
 
-	session_start();
-	if(!$_SESSION['_sfm_allowed']) {
-		// sha1, and random bytes to thwart timing attacks.  Not meant as secure hashing.
-		$t = bin2hex(openssl_random_pseudo_bytes(10));	
-		if($_POST['p'] && sha1($t.$_POST['p']) === sha1($t.$PASSWORD)) {
-			$_SESSION['_sfm_allowed'] = true;
-			header('Location: ?');
-		}
-		echo '<html><body><form action=? method=post>PASSWORD:<input type=password name=p /></form></body></html>'; 
-		exit;
-	}
+    session_start();
+    if($_GET['do'] == 'logout')
+    {
+        session_destroy();
+        header('Location: ?');  
+        exit;
+    }
+
+    if(isset($_SESSION['_sfm_allowed']))
+    {
+        if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1800)) {
+            // last request was more than 30 minutes ago
+            session_unset();     // unset $_SESSION variable for the run-time 
+            session_destroy();   // destroy session data in storage
+        }
+        $_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
+    }
+
+    if(!$_SESSION['_sfm_allowed']) {
+        // sha1, and random bytes to thwart timing attacks.  Not meant as secure hashing.
+        $t = bin2hex(openssl_random_pseudo_bytes(10));    
+        if($_POST['p'] && sha1($t.$_POST['p']) === sha1($t.$PASSWORD)) {
+            $_SESSION['_sfm_allowed'] = true;
+            header('Location: ?');
+        }
+        echo '<html><body><form action=? method=post>PASSWORD:<input type=password name=p /></form></body></html>'; 
+        exit;
+    }
 }
 
 // must be in UTF-8 or `basename` doesn't work
 setlocale(LC_ALL,'en_US.UTF-8');
 
-$tmp_dir = dirname($_SERVER['SCRIPT_FILENAME']);
-if(DIRECTORY_SEPARATOR==='\\') $tmp_dir = str_replace('/',DIRECTORY_SEPARATOR,$tmp_dir);
-$tmp = get_absolute_path($tmp_dir . '/' .$_REQUEST['file']);
-
-if($tmp === false)
-	err(404,'File or Directory Not Found');
-if(substr($tmp, 0,strlen($tmp_dir)) !== $tmp_dir)
-	err(403,"Forbidden");
-if(strpos($_REQUEST['file'], DIRECTORY_SEPARATOR) === 0) 
-	err(403,"Forbidden");
-
-
 if(!$_COOKIE['_sfm_xsrf'])
-	setcookie('_sfm_xsrf',bin2hex(openssl_random_pseudo_bytes(16)));
+    setcookie('_sfm_xsrf',bin2hex(openssl_random_pseudo_bytes(16)));
 if($_POST) {
-	if($_COOKIE['_sfm_xsrf'] !== $_POST['xsrf'] || !$_POST['xsrf'])
-		err(403,"XSRF Failure");
+    if($_COOKIE['_sfm_xsrf'] !== $_POST['xsrf'] || !$_POST['xsrf'])
+        err(403,"XSRF Failure");
 }
 
-$file = $_REQUEST['file'] ?: '.';
+if(DIRECTORY_SEPARATOR==='\\') $rootFolder = str_replace('/',DIRECTORY_SEPARATOR,$rootFolder);
+
+if(strpos($_REQUEST['file'], '..') !== false || strpos($_REQUEST['file'], './') !== false) 
+    err(403,"Forbidden");
+
+$tmp = get_absolute_path($rootFolder . '/' .$_REQUEST['file']);    
+if($tmp === false)
+    err(404,'File or Directory Not Found');
+    
+$file = getFilename($_REQUEST['file'], $rootFolder);
+
 if($_GET['do'] == 'list') {
-	if (is_dir($file)) {
-		$directory = $file;
-		$result = [];
-		$files = array_diff(scandir($directory), ['.','..']);
-	    foreach($files as $entry) if($entry !== basename(__FILE__) && !in_array(strtolower(pathinfo($entry, PATHINFO_EXTENSION)), $hidden_extensions)) {
-    		$i = $directory . '/' . $entry;
-	    	$stat = stat($i);
-	        $result[] = [
-	        	'mtime' => $stat['mtime'],
-	        	'size' => $stat['size'],
-	        	'name' => basename($i),
-	        	'path' => preg_replace('@^\./@', '', $i),
-	        	'is_dir' => is_dir($i),
-	        	'is_deleteable' => $allow_delete && ((!is_dir($i) && is_writable($directory)) ||
-                                                           (is_dir($i) && is_writable($directory) && is_recursively_deleteable($i))),
-	        	'is_readable' => is_readable($i),
-	        	'is_writable' => is_writable($i),
-	        	'is_executable' => is_executable($i),
-	        ];
-	    }
-	} else {
-		err(412,"Not a Directory");
-	}
-	echo json_encode(['success' => true, 'is_writable' => is_writable($file), 'results' =>$result]);
-	exit;
-} elseif ($_POST['do'] == 'delete') {
-	if($allow_delete) {
-		rmrf($file);
-	}
-	exit;
-} elseif ($_POST['do'] == 'mkdir' && $allow_create_folder== true) {
-	// don't allow actions outside root. we also filter out slashes to catch args like './../outside'
-	$dir = $_POST['name'];
-	$dir = str_replace('/', '', $dir);
-	if(substr($dir, 0, 2) === '..')
-	    exit;
-	chdir($file);
-	@mkdir($_POST['name']);
-	exit;
-} elseif ($_POST['do'] == 'upload' && $allow_upload == true) {
-	var_dump($_POST);
-	var_dump($_FILES);
-	var_dump($_FILES['file_data']['tmp_name']);
-	foreach($disallowed_extensions as $ext) 
-		if(preg_match(sprintf('/\.%s$/',preg_quote($ext)), $_FILES['file_data']['name'])) 
-			err(403,"Files of this type are not allowed.");
 
-	var_dump(move_uploaded_file($_FILES['file_data']['tmp_name'], $file.'/'.$_FILES['file_data']['name']));
-	exit;
+    if (is_dir($file)) {
+        $directory = $file;
+        $result = [];
+        $files = array_diff(scandir($directory), ['.','..']);
+        foreach($files as $entry) if($entry !== basename(__FILE__) && !in_array(strtolower(pathinfo($entry, PATHINFO_EXTENSION)), $hidden_extensions)) {
+            $i = $directory . '/' . $entry;
+            $stat = stat($i);
+            $result[] = [
+                'mtime' => $stat['mtime'],
+                'size' => $stat['size'],
+                'name' => basename($entry),
+                'path' => preg_replace('@^\./@','',substr($i, strlen($rootFolder))),
+                'is_dir' => is_dir($i),
+                'is_deleteable' => $allow_delete && ((!is_dir($i) && is_writable($directory)) ||
+                                                           (is_dir($i) && is_writable($directory) && is_recursively_deleteable($i))),
+                'is_readable' => is_readable($i),
+                'is_writable' => is_writable($i),
+                'is_executable' => is_executable($i),
+            ];
+        }
+    } else {
+        err(412,"Not a Directory");
+    }
+    echo json_encode(['success' => true, 'is_writable' => is_writable($file), 'results' =>$result]);
+    exit;
+} elseif ($_POST['do'] == 'delete') {
+    if($allow_delete) {
+        rmrf($file);
+    }
+    exit;
+} elseif ($_POST['do'] == 'mkdir' && $allow_create_folder== true) {
+    // don't allow actions outside root. we also filter out slashes to catch args like './../outside'
+    $dir = $_POST['name'];
+    $dir = str_replace('/', '', $dir);
+    if(substr($dir, 0, 2) === '..')
+        exit;
+    chdir($file);
+    @mkdir($_POST['name']);
+    exit;
+} elseif ($_POST['do'] == 'upload' && $allow_upload == true) {
+    var_dump($_POST);
+    var_dump($_FILES);
+    var_dump($_FILES['file_data']['tmp_name']);
+    foreach($disallowed_extensions as $ext) 
+        if(preg_match(sprintf('/\.%s$/',preg_quote($ext)), $_FILES['file_data']['name'])) 
+            err(403,"Files of this type are not allowed.");
+
+    var_dump(move_uploaded_file($_FILES['file_data']['tmp_name'], $file.'/'.$_FILES['file_data']['name']));
+    exit;
 } elseif ($_GET['do'] == 'download') {
-	$filename = basename($file);
-	header('Content-Type: ' . mime_content_type($file));
-	header('Content-Length: '. filesize($file));
-	header(sprintf('Content-Disposition: attachment; filename=%s',
-		strpos('MSIE',$_SERVER['HTTP_REFERER']) ? rawurlencode($filename) : "\"$filename\"" ));
-	ob_flush();
-	readfile($file);
-	exit;
+    $filename = basename($file);
+    header('Content-Type: ' . mime_content_type($file));
+    header('Content-Length: '. filesize($file));
+    header(sprintf('Content-Disposition: attachment; filename=%s',
+        strpos('MSIE',$_SERVER['HTTP_REFERER']) ? rawurlencode($filename) : "\"$filename\"" ));
+    ob_flush();
+    readfile($file);
+    exit;
 }
+
+function getFilename($tempFile, $baseFolder)
+{
+    if($tempFile)
+    {
+        if(strpos($tempFile, DIRECTORY_SEPARATOR) === 0)
+            $tempFile = substr($tempFile, 1);
+        return $baseFolder . '/' . $tempFile;
+    }    
+    return $baseFolder;
+}
+
 function rmrf($dir) {
-	if(is_dir($dir)) {
-		$files = array_diff(scandir($dir), ['.','..']);
-		foreach ($files as $file)
-			rmrf("$dir/$file");
-		rmdir($dir);
-	} else {
-		unlink($dir);
-	}
+    if(is_dir($dir)) {
+        $files = array_diff(scandir($dir), ['.','..']);
+        foreach ($files as $file)
+            rmrf("$dir/$file");
+        rmdir($dir);
+    } else {
+        unlink($dir);
+    }
 }
 function is_recursively_deleteable($d) {
-	$stack = [$d];
-	while($dir = array_pop($stack)) {
-		if(!is_readable($dir) || !is_writable($dir)) 
-			return false;
-		$files = array_diff(scandir($dir), ['.','..']);
-		foreach($files as $file) if(is_dir($file)) {
-			$stack[] = "$dir/$file";
-		}
-	}
-	return true;
+    $stack = [$d];
+    while($dir = array_pop($stack)) {
+        if(!is_readable($dir) || !is_writable($dir)) 
+            return false;
+        $files = array_diff(scandir($dir), ['.','..']);
+        foreach($files as $file) if(is_dir($file)) {
+            $stack[] = "$dir/$file";
+        }
+    }
+    return true;
 }
 
 // from: http://php.net/manual/en/function.realpath.php#84012
@@ -159,15 +193,15 @@ function get_absolute_path($path) {
     }
 
 function err($code,$msg) {
-	http_response_code($code);
-	echo json_encode(['error' => ['code'=>intval($code), 'msg' => $msg]]);
-	exit;
+    http_response_code($code);
+    echo json_encode(['error' => ['code'=>intval($code), 'msg' => $msg]]);
+    exit;
 }
 
 function asBytes($ini_v) {
-	$ini_v = trim($ini_v);
-	$s = ['g'=> 1<<30, 'm' => 1<<20, 'k' => 1<<10];
-	return intval($ini_v) * ($s[strtolower(substr($ini_v,-1))] ?: 1);
+    $ini_v = trim($ini_v);
+    $s = ['g'=> 1<<30, 'm' => 1<<20, 'k' => 1<<10];
+    return intval($ini_v) * ($s[strtolower(substr($ini_v,-1))] ?: 1);
 }
 $MAX_UPLOAD_SIZE = min(asBytes(ini_get('post_max_size')), asBytes(ini_get('upload_max_filesize')));
 ?>
@@ -183,8 +217,9 @@ th .indicator {margin-left: 6px }
 thead {border-top: 1px solid #82CFFA; border-bottom: 1px solid #96C4EA;border-left: 1px solid #E7F2FB;
 	border-right: 1px solid #E7F2FB; }
 #top {height:52px;}
-#mkdir {display:inline-block;float:right;padding-top:16px;}
-label { display:block; font-size:11px; color:#555;}
+#mkdir {display:inline-block;float:right;}
+#last-access {display:inline-block;float:left;}
+label {font-size:11px; color:#555;}
 #file_drop_target {width:500px; padding:12px 0; border: 4px dashed #ccc;font-size:12px;color:#ccc;
 	text-align: center;float:right;margin-right:20px;}
 #file_drop_target.drag_over {border: 4px dashed #96C4EA; color: #96C4EA;}
@@ -380,7 +415,7 @@ $(function(){
 	}
 	function renderFileRow(data) {
 		var $link = $('<a class="name" />')
-			.attr('href', data.is_dir ? '#' + data.path : './'+data.path)
+			.attr('href', data.is_dir ? '#' + data.path : '?do=download&file='+encodeURIComponent(data.path))
 			.text(data.name);
 		var allow_direct_link = <?php echo $allow_direct_link?'true':'false'; ?>;
         	if (!data.is_dir && !allow_direct_link)  $link.css('pointer-events','none');
@@ -432,15 +467,6 @@ $(function(){
 </head><body>
 <div id="top">
    <?php if($allow_upload == true): ?>
-	<form action="?" method="post" id="mkdir" />
-		<label for=dirname>Create New Folder</label><input id=dirname type=text name=name value="" />
-		<input type="submit" value="create" />
-	</form>
-
-   <?php endif; ?>
-
-   <?php if($allow_upload == true): ?>
-
 	<div id="file_drop_target">
 		Drag Files Here To Upload
 		<b>or</b>
@@ -451,6 +477,18 @@ $(function(){
 </div>
 
 <div id="upload_progress"></div>
+<div>
+	<div id="last-access">
+	    <span>Last access: <?php  echo date("d-m-Y H:i:s"); ?></span>
+		<?php  if($PASSWORD) { ?> <a href='?do=logout'> Logout</a> <?php } ?>
+	</div>
+   <?php if($allow_upload == true): ?>
+	<form action="?" method="post" id="mkdir" />
+		<label for=dirname>Create New Folder</label><input id=dirname type=text name=name value="" />
+		<input type="submit" value="create" />
+	</form>
+   <?php endif; ?>   
+ </div>
 <table id="table"><thead><tr>
 	<th>Name</th>
 	<th>Size</th>
